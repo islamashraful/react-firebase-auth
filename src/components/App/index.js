58af9eb814046d5063e2da1a5d810e7d9bada8eb
Ashraful Islam <rx.ju.edu@gmail.com>
2021-07-07 20:47:37 +0700

    :wrench: Navbar based on login status

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navigation from "../Navigation";
 import LandingPage from "../Landing";
@@ -9,21 +9,39 @@ import HomePage from "../Home";
 import AccountPage from "../Account";
 import AdminPage from "../Admin";
 import * as ROUTES from "../../constants/routes";
+import { FirebaseContext } from "../Firebase";
+import { AuthUserContext } from "../Session";
 
-const App = () => (
-  <Router>
-    <div>
-      <Navigation />
-      <hr />
-      <Route exact path={ROUTES.LANDING} component={LandingPage} />
-      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-      <Route path={ROUTES.HOME} component={HomePage} />
-      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-      <Route path={ROUTES.ADMIN} component={AdminPage} />
-    </div>
-  </Router>
-);
+const App = () => {
+  const [authUser, setAuthUser] = useState({ authUser: null });
+  const firebase = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const listener = firebase.auth.onAuthStateChanged((res) => {
+      res ? setAuthUser({ authUser: res }) : setAuthUser({ authUser: null });
+    });
+    return () => {
+      listener();
+    };
+  }, [firebase]);
+
+  return (
+    <AuthUserContext.Provider value={authUser}>
+      <Router>
+        <div>
+          <Navigation />
+          <hr />
+          <Route exact path={ROUTES.LANDING} component={LandingPage} />
+          <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+          <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+          <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+          <Route path={ROUTES.HOME} component={HomePage} />
+          <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+          <Route path={ROUTES.ADMIN} component={AdminPage} />
+        </div>
+      </Router>
+    </AuthUserContext.Provider>
+  );
+};
 
 export default App;
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,27 +1,43 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import * as ROUTES from "../../constants/routes";
+import { AuthUserContext } from "../Session";
+import SignOutButton from "../SignOut";
 
-const Navigation = () => (
-  <div>
-    <ul>
-      <li>
-        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.LANDING}>Landing</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.HOME}>Home</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ACCOUNT}>Account</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ADMIN}>Admin</Link>
-      </li>
-    </ul>
-  </div>
-);
+const Navigation = () => {
+  const { authUser } = useContext(AuthUserContext);
+
+  return (
+    <div>
+      <ul>
+        {authUser ? (
+          <>
+            <li>
+              <Link to={ROUTES.LANDING}>Landing</Link>
+            </li>
+            <li>
+              <Link to={ROUTES.HOME}>Home</Link>
+            </li>
+            <li>
+              <Link to={ROUTES.ACCOUNT}>Account</Link>
+            </li>
+            <li>
+              <SignOutButton />
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to={ROUTES.LANDING}>Landing</Link>
+            </li>
+            <li>
+              <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </div>
+  );
+};
 
 export default Navigation;
