cf7346832868904c0a095d92bffa816c71306c43
Ashraful Islam <rx.ju.edu@gmail.com>
2021-07-07 14:26:15 +0700

    :wrench: SignUp

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,7 +1,92 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { FirebaseContext } from "../Firebase";
+import * as ROUTES from "../../constants/routes";
 
 const SignUp = () => {
-  return <p>Account</p>;
+  return (
+    <div>
+      <h1>SignUp</h1>
+      <SignUpForm />
+    </div>
+  );
+};
+
+const SignUpForm = () => {
+  const initialState = {
+    username: "",
+    email: "",
+    passwordOne: "",
+    passwordTwo: "",
+    error: null,
+  };
+  const [formState, setFormState] = useState(initialState);
+  const firebase = useContext(FirebaseContext);
+  const history = useHistory();
+
+  const handleSubmit = (event) => {
+    firebase
+      .doCreateUserWithEmailAndPassword(formState.email, formState.passwordOne)
+      .then((authUser) => {
+        setFormState(initialState);
+        history.push(ROUTES.HOME);
+      })
+      .catch((error) => {
+        setFormState((prevState) => ({ ...prevState, ...{ error } }));
+      });
+    event.preventDefault();
+  };
+
+  const handleChange = (event) => {
+    setFormState((prevState) => ({
+      ...prevState,
+      ...{ [event.target.name]: event.target.value },
+    }));
+  };
+
+  const isInvalid =
+    formState.passwordOne !== formState.passwordTwo ||
+    formState.passwordOne === "" ||
+    formState.email === "" ||
+    formState.username === "";
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        name="username"
+        value={formState.username}
+        onChange={handleChange}
+        type="text"
+        placeholder="Full Name"
+      />
+      <input
+        name="email"
+        value={formState.email}
+        onChange={handleChange}
+        type="text"
+        placeholder="Email Address"
+      />
+      <input
+        name="passwordOne"
+        value={formState.passwordOne}
+        onChange={handleChange}
+        type="password"
+        placeholder="Password"
+      />
+      <input
+        name="passwordTwo"
+        value={formState.passwordTwo}
+        onChange={handleChange}
+        type="password"
+        placeholder="Confirm Password"
+      />
+      <button disabled={isInvalid} type="submit">
+        Sign Up
+      </button>
+
+      {formState.error && <p>{formState.error.message}</p>}
+    </form>
+  );
 };
 
 export default SignUp;
